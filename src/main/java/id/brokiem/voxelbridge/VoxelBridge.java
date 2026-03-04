f3487655885043a460f82db4d9680818f04f2d86
brokiem <milyhqvideo@gmail.com>
2026-03-04 23:05:23 +0800

    Create maven core project structure

---
+++ src/main/java/id/brokiem/voxelbridge/VoxelBridge.java
@@ -0,0 +1,10 @@
+package id.brokiem.voxelbridge;
+
+/**
+ * Hello world!
+ */
+public class VoxelBridge {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
