d20d271802e27a09e76614bdfb1c8bf3e759d2ab
brokiem <milyhqvideo@gmail.com>
2026-03-14 23:46:41 +0800

    Don't translate keep alive before play state

--- src/main/java/id/brokiem/voxelbridge/translation/impl/LceKeepAliveTranslator.java
+++ src/main/java/id/brokiem/voxelbridge/translation/impl/LceKeepAliveTranslator.java
@@ -2,6 +2,7 @@ package id.brokiem.voxelbridge.translation.impl;
 
 import id.brokiem.voxelbridge.protocol.java.packets.play.JavaServerboundKeepAlivePacket;
 import id.brokiem.voxelbridge.protocol.lce.packets.LceKeepAlivePacket;
+import id.brokiem.voxelbridge.session.ConnectionState;
 import id.brokiem.voxelbridge.session.Session;
 import id.brokiem.voxelbridge.translation.ServerboundTranslator;
 import id.brokiem.voxelbridge.translation.TranslationResult;
@@ -9,6 +10,10 @@ import id.brokiem.voxelbridge.translation.TranslationResult;
 public class LceKeepAliveTranslator implements ServerboundTranslator<LceKeepAlivePacket> {
     @Override
     public TranslationResult translate(LceKeepAlivePacket input, Session session) {
+        if (!session.getState().equals(ConnectionState.PLAY)) {
+            return TranslationResult.empty();
+        }
+
         JavaServerboundKeepAlivePacket java = new JavaServerboundKeepAlivePacket();
         java.setKeepAliveId(input.getKeepAliveId());
         return TranslationResult.toServer(java);
